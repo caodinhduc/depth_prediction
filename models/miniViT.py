2e6f5a04af014b7df7e4bcf0453f9efb76fff14b
duccd <caodinhducuet@gmail.com>
2021-10-16 16:53:47 +0900

    ok

--- models/miniViT.py
+++ models/miniViT.py
@@ -20,20 +20,14 @@ class mViT(nn.Module):
                                        nn.Linear(256, 256),
                                        nn.LeakyReLU(),
                                        nn.Linear(256, dim_out))
-        self.FFC = FFC(in_channels=128, out_channels=128, kernel_size=3, padding=1, ratio_gin=0.5, ratio_gout=0.5)
+
 
     def forward(self, x):
         # n, c, h, w = x.size()
         tgt = self.patch_transformer(x.clone())  # .shape = S, N, E
 
         # 2, 128, 176, 352
-        # add fourier layer
-        x_l, x_g = self.FFC((x[:, :64, :, :], x[:, 64:, :, :]))
-        fourier = torch.cat((x_l, x_g), dim=1)
-        x = x + fourier
         x = self.conv3x3(x)
-
-
         regression_head, queries = tgt[0, ...], tgt[1:self.n_query_channels + 1, ...]
 
         # Change from S, N, E to N, S, E
